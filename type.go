800a00a1e8fad4091feea783d62766ede0faae26
isnaenti <isnaenti16@gmail.com>
2023-11-17 21:29:00 +0700

    push

---
+++ type.go
@@ -0,0 +1,13 @@
+package webhook
+
+type Reply struct {
+	Message string `bson:"messsage"`
+}
+
+var stringreply = []string{
+	"hai hai hai kaka lagi ngapain",
+	"tebak tebak lokasi yo",
+	"bentar dulu sabar napa",
+	"coba kirim lokasi kamu deh",
+	"maap isnaentinya ngga ada",
+}
